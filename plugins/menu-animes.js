133d02759e2cea39b95d6edf67353a4bf386ecf4
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2023-12-12 20:16:11 -0500

    🥝 𝗞𝗶𝘄𝗶 𝗕𝗼𝘁 🥝

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -18,30 +18,30 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = ` 🦅 𝘽𝙤𝙩 𝙁𝙚𝙣𝙞𝙭 🦅
+    const str = ` 🥝 𝗞𝗶𝘄𝗶 𝗕𝗼𝘁 🥝
     
-➤ *Hola, ${taguser} Recuerda Que Para Que Funcionen El Bot Debe Ser Admin Del Grupo 🦅*
+➤ *Hola, ${taguser} Recuerda Que Para Que Funcionen El Bot Debe Ser Admin Del grupo 🥝*
 
  ◤━━━━━━━━━━━◥
- ►🦅 _${usedPrefix}LigaMixta5_
- ►🦅 _${usedPrefix}Liga5_
- ►🦅 _${usedPrefix}LigaMixta_
- ►🦅 _${usedPrefix}Liga_
- ►🦅 _${usedPrefix}ScrimMixta_
- ►🦅 _${usedPrefix}Scrim_
- ►🦅 _${usedPrefix}Masc4_
- ►🦅 _${usedPrefix}Masc8_
- ►🦅 _${usedPrefix}Masc12_
- ►🦅 _${usedPrefix}Masc16_
- ►🦅 _${usedPrefix}Masc20_
- ►🦅 _${usedPrefix}Masc24_
- ►🦅 _${usedPrefix}Fem4_
- ►🦅 _${usedPrefix}Fem8_
- ►🦅 _${usedPrefix}Fem12_
- ►🦅 _${usedPrefix}Fem16_
- ►🦅 _${usedPrefix}Fem20_
- ►🦅 _${usedPrefix}Fem24_
- ►🦅 _${usedPrefix}Mapa_
+ ►🥝 _${usedPrefix}LigaMixta5_
+ ►🥝 _${usedPrefix}Liga5_
+ ►🥝 _${usedPrefix}LigaMixta_
+ ►🥝 _${usedPrefix}Liga_
+ ►🥝 _${usedPrefix}ScrimMixta_
+ ►🥝 _${usedPrefix}Scrim_
+ ►🥝 _${usedPrefix}Masc4_
+ ►🥝 _${usedPrefix}Masc8_
+ ►🥝 _${usedPrefix}Masc12_
+ ►🥝 _${usedPrefix}Masc16_
+ ►🥝 _${usedPrefix}Masc20_
+ ►🥝 _${usedPrefix}Masc24_
+ ►🥝 _${usedPrefix}Fem4_
+ ►🥝 _${usedPrefix}Fem8_
+ ►🥝 _${usedPrefix}Fem12_
+ ►🥝 _${usedPrefix}Fem16_
+ ►🥝 _${usedPrefix}Fem20_
+ ►🥝 _${usedPrefix}Fem24_
+ ►🥝 _${usedPrefix}Mapa_
  ◣━━━━━━━━━━━◢`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
